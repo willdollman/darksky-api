429f9c494207ae35c43189288b707a1991910b98
Will Dollman <will@dollman.org>
2013-05-29 20:01:17 +0100

    Refactor into subroutines to make more extensible

--- willitraininthenexthour/forecast.pl
+++ willitraininthenexthour/forecast.pl
@@ -9,58 +9,85 @@ use JSON;
 use Mojo::UserAgent;
 use Scalar::Util qw(looks_like_number);
 
-my $local_file = "./api-responses/test1.json";
+my $local_file = "./api-responses/bristol-20130529-1955.json";
 
 # Quick config
 my $use_cache = 0; # use a cached API response to for debugging
 my $debug     = 0; # show debug messages
-my $api_key   = "your API key here";
+my $api_key   = "your api key here";
 $\ = "\n";
 
 print "Content-type: text/html\n\n";
 
+my ($location, $is_web_request) = get_location();
+my $weather = get_weather($location);
+my $is_rain = is_rain_next_hour($weather);
+
+print to_json( { "rain" => "$is_rain" } ) if $is_web_request;
+
 # Get latitude and longitude
-my %query = parse_query_string();
-if ( !looks_like_number($query{lat}) || !looks_like_number($query{lon}) ) {
-    print to_json( { "error" => "invalid latitude/longitude" });
-    die "Invalid location ($query{lat}/$query{lon})";
+sub get_location {
+    my %query;
+    my $is_web_request;
+
+    if (%query = parse_query_string()) {
+        $is_web_request = 1;
+
+        if ( !looks_like_number($query{lat}) || !looks_like_number($query{lon}) ) {
+            print to_json( { "error" => "invalid latitude/longitude" });
+            die "Invalid location ($query{lat}/$query{lon})";
+        }
+    }
+    else {
+        (($query{lat}, $query{lon}) = @ARGV) || die "Incorrect arguments";
+        $is_web_request = 0;
+    }
+    my $location = "$query{lat},$query{lon}";
+
+    return ($location, $is_web_request);
 }
-my $location = "$query{lat},$query{lon}";
 
 # Get some weather data
-my $weather;
-if ($use_cache) {
-    print "[using local cache]" if $debug;
-    open(FILE, $local_file);
-    $weather = from_json(<FILE>);
-}
-else {
-    print "[fetching from darksky]" if $debug;
-    my $ua = Mojo::UserAgent->new;
-    $weather = $ua->get('https://api.forecast.io/forecast/' . $api_key . '/' . $location)->res->json;
-}
+sub get_weather {
+    my $location = shift @_;
 
-# check that the response contains hyperlocal weather data
-if (!defined $weather->{minutely}->{data}) {
-    print to_json( { "error" => "unable to get hyperlocal forecast - perhaps your area is not yet supported?" } );
-    die "Can't get hyperlocal forecast data from response";
-}
-
-my $is_rain = 0;
-foreach my $minute (@{$weather->{minutely}->{data}}) {
-    print "<br>data: " . $minute->{time} . " at " . $minute->{precipIntensity} if $debug;
-    if ($minute->{precipIntensity} > 0 && !$is_rain) {
-        print "<br>Yes. It will rain at " . scalar localtime($minute->{time}) . "<br>" if $debug;
-        $is_rain = 1;
+    my $weather;
+    if ($use_cache) {
+        print "[using local cache]" if $debug;
+        open(FILE, $local_file);
+        $weather = from_json(<FILE>);
     }
+    else {
+        print "[fetching from darksky]" if $debug;
+        my $ua = Mojo::UserAgent->new;
+        $weather = $ua->get('https://api.forecast.io/forecast/' . $api_key . '/' . $location)->res->json;
+        print "location is $location\n";
+    }
+
+    # check that the response contains hyperlocal weather data
+    if (!defined $weather->{minutely}->{data}) {
+        print to_json( { "error" => "unable to get hyperlocal forecast - perhaps your area is not yet supported?" } );
+        die "Can't get hyperlocal forecast data from response";
+    }
+
+    return $weather;
 }
 
-if ( !$is_rain ) {
-    print "<br>No rain expected - time to go outside<br>" if $debug;
+# Will it rain in the next hour?
+sub is_rain_next_hour {
+    my $weather = shift @_;
+
+    my $is_rain = 0;
+    foreach my $minute (@{$weather->{minutely}->{data}}) {
+        print "<br>data: " . $minute->{time} . " at " . $minute->{precipIntensity} if $debug;
+        if ($minute->{precipIntensity} > 0 && !$is_rain) {
+            print "<br>Yes. It will rain at " . scalar localtime($minute->{time}) . "<br>" if $debug;
+            $is_rain = 1;
+        }
+    }
+
+    return $is_rain;
 }
-
-print to_json( { "rain" => "$is_rain" } );
-
 
 # feels like there should be a builtin for this...
 sub parse_query_string {
