9629fe8a2ac793f524e72a52bf82be1ca4ae8fd4
Will Dollman <will@dollman.org>
2013-05-30 22:06:05 +0100

    Cleanup web API for deployment

--- willitraininthenexthour/forecast.pl
+++ willitraininthenexthour/forecast.pl
@@ -3,8 +3,6 @@
 use warnings;
 use strict;
 
-use lib '/home/will/Dropbox/code/darksky/';
-
 use Weather::ForecastIO qw( is_web_request get_location get_weather is_rain_next_hour );
 use JSON;
 
@@ -15,16 +13,9 @@ $\ = "\n";
 
 print "Content-type: text/html\n\n";
 
-# this is only being called from the web, so the is_web_request stuff is a bit unecessary
-
 my $is_web_request = is_web_request();
 my $location = get_location(\@ARGV, $is_web_request);
-my $weather = get_weather($location, $api_key, $local_file);
-my $is_rain = is_rain_next_hour($weather);
+my $weather  = get_weather($location, $api_key);
+my $is_rain  = is_rain_next_hour($weather);
 
-my $time = 21;
-my ($precip, $summary) = get_weather_at_hour($weather, $time);
-print "At $time it'll be ", lc $summary, " ($precip)\n";
-
-print to_json( { "rain" => "$is_rain" } ) if $is_web_request;
-#print "Yes, it will rain in the next hour" if $debug && $is_rain;
+print to_json( { "rain" => "$is_rain" } );
