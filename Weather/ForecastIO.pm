05b3c3c4ac84e1ef2ec7f8af95e3dec56568ffa2
Will Dollman <will@dollman.org>
2013-05-30 20:56:37 +0100

    Create Weather::ForecastIO - break out reusable functionality

---
+++ Weather/ForecastIO.pm
@@ -0,0 +1,150 @@
+#!/usr/bin/perl
+
+package Weather::ForecastIO;
+
+use warnings;
+use strict;
+
+use Carp;
+use Data::Dumper;
+use DateTime;
+use Exporter qw( import );
+use IO::Socket::SSL;
+use JSON;
+use Mojo::UserAgent;
+use Readonly;
+use Scalar::Util qw(looks_like_number);
+
+our @EXPORT_OK = qw( is_web_request get_location get_weather is_rain_next_hour get_weather_at_hour hour_to_epoch parse_query_string );
+
+# Quick config
+Readonly my $DEBUG => 1; # show debug messages
+$\ = "\n";
+
+# could use an error-printing function which croaks/returns json based on is_web_request
+
+sub is_web_request {
+    return (parse_query_string() ? 1 : 0);
+}
+
+# Get latitude and longitude
+sub get_location {
+    my $ARGV = shift @_;
+    my $is_web_request = shift @_;
+    my %query;
+
+    if ($is_web_request) {
+        %query = parse_query_string();
+        if ( !looks_like_number($query{lat}) || !looks_like_number($query{lon}) ) {
+            print to_json( { "error" => "invalid latitude/longitude" });
+            croak "Invalid location ($query{lat}/$query{lon})";
+        }
+    }
+    else {
+        (($query{lat}, $query{lon}) = @{$ARGV}) || croak "Incorrect arguments (@{$ARGV})";
+        if ( !looks_like_number($query{lat}) || !looks_like_number($query{lon}) ) {
+            croak "Invalid latitude/longitude\n";
+        }
+    }
+    my $location = "$query{lat},$query{lon}";
+
+    return $location;
+}
+
+# Get some weather data
+sub get_weather {
+    my $location = shift @_ || croak "No location supplied";
+    my $api_key  = shift @_ || croak "No API key supplied";
+    my $local_file = shift @_;
+
+    my $weather;
+    if ($local_file) {
+        print "[using local cache]" if $DEBUG;
+        open(FILE, $local_file);
+        $weather = from_json(<FILE>);
+    }
+    else {
+        print "[fetching from darksky]" if $DEBUG;
+        my $ua = Mojo::UserAgent->new;
+        $weather = $ua->get('https://api.forecast.io/forecast/' . $api_key . '/' . $location)->res->json;
+        print "location is $location\n";
+    }
+
+    # check that the response contains hyperlocal weather data
+    if (!defined $weather->{minutely}->{data}) {
+        print to_json( { "error" => "Unable to get hyperlocal forecast - perhaps your area is not yet supported?" } );
+        croak "Can't get hyperlocal forecast data from response";
+    }
+
+    return $weather;
+}
+
+# Will it rain in the next hour?
+sub is_rain_next_hour {
+    my $weather = shift @_;
+
+    my $is_rain = 0;
+    foreach my $minute (@{$weather->{minutely}->{data}}) {
+        print "<br>data: " . $minute->{time} . " at " . $minute->{precipIntensity} if $DEBUG == 2;
+        if ($minute->{precipIntensity} > 0 && !$is_rain) {
+            print "<br>Yes. It will rain at " . scalar localtime($minute->{time}) . "<br>" if $DEBUG;
+            $is_rain = 1;
+        }
+    }
+
+    return $is_rain;
+}
+
+sub get_weather_at_hour {
+    my $weather = shift @_;
+    my $target_hour = shift @_;
+    my $target_epoch = hour_to_epoch($target_hour);
+
+    foreach my $hour (@{$weather->{hourly}->{data}}) {
+        print "found " . $hour->{time} . ": precip is ". $hour->{precipIntensity} . " (" . $hour->{summary} . ")"
+            if $hour->{time} == $target_epoch;
+        return ($hour->{precipIntensity}, $hour->{summary})
+            if $hour->{time} == $target_epoch;
+    }
+    carp "Could not find $target_hour in response";
+    return (-1, -1);
+}
+
+# Convert integer hour to unix epoch timestamp
+# eg 17 -> 17:00 today
+# will return a timestamp for tomorrow if input hour has already passed
+sub hour_to_epoch {
+    my $target_hour = shift @_;
+    croak "$target_hour is not between 0 and 23" if ($target_hour < 0 || $target_hour > 23);
+
+    # DateTime handles BST. Phew!
+    my $timezone = 'Europe/London';
+    my $dt = DateTime->now( time_zone => $timezone );
+
+    # You probably mean for tomorrow
+    if ($target_hour < $dt->hour) {
+        $dt->add( days => 1 );
+    }
+
+    # Set time to the target hour
+    $dt->set_hour($target_hour);
+    $dt->set_minute(0);
+    $dt->set_second(0);
+
+    return $dt->epoch;
+}
+
+# feels like there should be a builtin for this...
+sub parse_query_string {
+    my %in;
+    if (length ($ENV{'QUERY_STRING'}) > 0){
+        my $buffer = $ENV{'QUERY_STRING'};
+        my @pairs = split(/&/, $buffer);
+        foreach my $pair (@pairs){
+            my ($name, $value) = split(/=/, $pair);
+            $value =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
+            $in{$name} = $value; 
+        }
+    }
+    return %in;
+}
--- willitraininthenexthour/forecast.pl
+++ willitraininthenexthour/forecast.pl
@@ -3,144 +3,28 @@
 use warnings;
 use strict;
 
-use Data::Dumper;
-use DateTime;
-use IO::Socket::SSL;
-use JSON;
-use Mojo::UserAgent;
-use Scalar::Util qw(looks_like_number);
+use lib '/home/will/Dropbox/code/darksky/';
 
-my $local_file = "./api-responses/bristol-20130529-1955.json";
+use Weather::ForecastIO qw( is_web_request get_location get_weather is_rain_next_hour );
+use JSON;
 
 # Quick config
-my $use_cache = 0; # use a cached API response to for debugging
-my $debug     = 0; # show debug messages
 my $api_key   = "your api key here";
+my $local_file = "./api-responses/bristol-20130529-1955.json";
 $\ = "\n";
 
 print "Content-type: text/html\n\n";
 
+# this is only being called from the web, so the is_web_request stuff is a bit unecessary
+
 my $is_web_request = is_web_request();
-my $location = get_location($is_web_request);
-my $weather = get_weather($location);
+my $location = get_location(\@ARGV, $is_web_request);
+my $weather = get_weather($location, $api_key, $local_file);
 my $is_rain = is_rain_next_hour($weather);
 
+my $time = 21;
+my ($precip, $summary) = get_weather_at_hour($weather, $time);
+print "At $time it'll be ", lc $summary, " ($precip)\n";
+
 print to_json( { "rain" => "$is_rain" } ) if $is_web_request;
-print "Yes, it will rain in the next hour" if $debug && $is_rain;
-
-sub is_web_request {
-    return (parse_query_string() ? 1 : 0);
-}
-
-# Get latitude and longitude
-sub get_location {
-    my $is_web_request = shift @_;
-    my %query;
-
-    if ($is_web_request) {
-        %query = parse_query_string();
-        if ( !looks_like_number($query{lat}) || !looks_like_number($query{lon}) ) {
-            print to_json( { "error" => "invalid latitude/longitude" });
-            die "Invalid location ($query{lat}/$query{lon})";
-        }
-    }
-    else {
-        (($query{lat}, $query{lon}) = @ARGV) || die "Incorrect arguments";
-    }
-    my $location = "$query{lat},$query{lon}";
-
-    return $location;
-}
-
-# Get some weather data
-sub get_weather {
-    my $location = shift @_;
-
-    my $weather;
-    if ($use_cache) {
-        print "[using local cache]" if $debug;
-        open(FILE, $local_file);
-        $weather = from_json(<FILE>);
-    }
-    else {
-        print "[fetching from darksky]" if $debug;
-        my $ua = Mojo::UserAgent->new;
-        $weather = $ua->get('https://api.forecast.io/forecast/' . $api_key . '/' . $location)->res->json;
-        print "location is $location\n";
-    }
-
-    # check that the response contains hyperlocal weather data
-    if (!defined $weather->{minutely}->{data}) {
-        print to_json( { "error" => "Unable to get hyperlocal forecast - perhaps your area is not yet supported?" } );
-        die "Can't get hyperlocal forecast data from response";
-    }
-
-    return $weather;
-}
-
-# Will it rain in the next hour?
-sub is_rain_next_hour {
-    my $weather = shift @_;
-
-    my $is_rain = 0;
-    foreach my $minute (@{$weather->{minutely}->{data}}) {
-        print "<br>data: " . $minute->{time} . " at " . $minute->{precipIntensity} if $debug == 2;
-        if ($minute->{precipIntensity} > 0 && !$is_rain) {
-            print "<br>Yes. It will rain at " . scalar localtime($minute->{time}) . "<br>" if $debug;
-            $is_rain = 1;
-        }
-    }
-
-    return $is_rain;
-}
-
-sub get_weather_at_hour {
-    my $weather = shift @_;
-    my $target_hour = shift @_;
-    my $target_epoch = hour_to_epoch($target_hour);
-
-    foreach my $hour (@{$weather->{hourly}->{data}}) {
-        print "found " . $hour->{time} . ": precip is ". $hour->{precipIntensity} . " (" . $hour->{summary} . ")"
-            if $hour->{time} == $target_epoch;
-        return ($hour->{precipIntensity}, $hour->{summary})
-            if $hour->{time} == $target_epoch;
-    }
-}
-
-# Convert integer hour to unix epoch timestamp
-# eg 17 -> 17:00 today
-# will return a timestamp for tomorrow if input hour has already passed
-sub hour_to_epoch {
-    my $target_hour = shift @_;
-
-    # DateTime handles BST. Phew!
-    my $timezone = 'Europe/London';
-    my $dt = DateTime->now( time_zone => $timezone );
-
-    # You probably mean for tomorrow
-    if ($target_hour < $dt->hour) {
-        $dt->add( days => 1 );
-    }
-
-    # Set time to the target hour
-    $dt->set_hour($target_hour);
-    $dt->set_minute(0);
-    $dt->set_second(0);
-
-    return $dt->epoch;
-}
-
-# feels like there should be a builtin for this...
-sub parse_query_string {
-    my %in;
-    if (length ($ENV{'QUERY_STRING'}) > 0){
-        my $buffer = $ENV{'QUERY_STRING'};
-        my @pairs = split(/&/, $buffer);
-        foreach my $pair (@pairs){
-            my ($name, $value) = split(/=/, $pair);
-            $value =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
-            $in{$name} = $value; 
-        }
-    }
-    return %in;
-}
+#print "Yes, it will rain in the next hour" if $debug && $is_rain;
